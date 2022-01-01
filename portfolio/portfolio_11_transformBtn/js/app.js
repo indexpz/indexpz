87e5d2b48dd27e34fed0322f81b1a671bee151c6
Paweł Zieliński <pawelz72@gmail.com>
2022-01-01 23:12:16 +0100

    cd portfolio

---
+++ portfolio/portfolio_11_transformBtn/js/app.js
@@ -0,0 +1,12 @@
+$(".larger").click(function(){
+    $("h1").toggleClass("large");
+    
+})
+
+$(".smaller").click(function(){
+    $("h1").toggleClass("small");
+})
+
+$(".border").click(function(){
+    $("h1").toggleClass("frame");
+})
