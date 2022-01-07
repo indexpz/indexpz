746bb5409c1df5ba6241098e1054f3a55d05863b
Paweł Zieliński <pawelz72@gmail.com>
2022-01-07 11:56:44 +0100

    JS & JQ

---
+++ portfolio/39_jsAddBoxAfterClick/js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const js = document.querySelector(".js");
+    const reset = document.querySelector(".reset");
+    const spaceForSquares = document.querySelector(".space-for-squares");
+    let nr = 0;
+
+    js.addEventListener("click", addSquare);
+
+
+    function addSquare() {
+        nr++;
+        const square = document.createElement("div");
+        // square.style.width = "100px";
+        // square.style.height = "100px";
+
+        square.style.textAlign = "center";
+        square.style.lineHeight = "100px";
+        square.style.fontSize = "30px";
+        square.style.fontWeight = "600";
+        square.style.color = "black";
+
+        // square.style.display = "inline-block";
+        // square.setAttribute("class", "test");
+        square.style.background = `hsl(${Math.random() * 360}, 100%, 90%)`;
+        square.classList.add(`box`);
+        square.innerHTML = `<p>${nr}</p>`
+        spaceForSquares.appendChild(square);
+    }
+
+    reset.addEventListener("click", () => {
+        spaceForSquares.innerHTML = "";
+    })
+
+});
---
+++ portfolio/39_jsAddBoxAfterClick/js/jqApp.js
@@ -0,0 +1,7 @@
+let nrJq = 1;
+
+$(`button.jq`).on('click', function () {
+    const bgColor = `hsl(${Math.random() * 360}, 50%, 30%)`;
+    $(`.space-for-squares`).append(`<div class="box-jq" style="background-color: ${bgColor}; color: white; text-align: center; line-height: 100px; font-size: 30px">${nrJq++} JQ`)
+});
+
